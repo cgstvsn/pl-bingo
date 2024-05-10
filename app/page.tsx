fa43fba6190c8f3bb738dbcfa847edc3bf6e960b
Flohhhhh <56054585+cgstvsn@users.noreply.github.com>
2024-05-10 12:39:31 -0400

    Readme and contributions

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { FaGithub } from "react-icons/fa";
 import Link from "next/link";
 import BingoCard from "@/components/bingo/BingoCard";
 
@@ -9,13 +10,16 @@ export default async function Index() {
       <div className="space-y-3">
         <h1 className="text-3xl font-bold text-gray-300">PL Lounge Bingo</h1>
         <p className="text-gray-400">Refresh to get a new card.</p>
+        <Link
+          href="https://github.com/cgstvsn/pl-bingo"
+          target="_blank"
+          className="flex items-center gap-2 mt-4 text-gray-400 hover:underline"
+        >
+          <FaGithub /> Contribute
+        </Link>
       </div>
 
       <BingoCard />
-
-      <Link href="https://github.com/cgstvsn/pl-bingo" target="_blank">
-        Contribute
-      </Link>
     </div>
   );
 }
